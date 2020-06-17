c8510ae539759d694dbfcf7d899d3f3459d4a2d9
AndikaSutejoPutro <dika_tejo@yahoo.com>
2020-06-17 20:16:25 +0700

    LoginPreferenceApp

---
+++ app/src/main/java/com/example/loginpreferenceapp/SepakBolaActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginpreferenceapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("Registered")
+public class SepakBolaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sepakbola);
+    }
+}
